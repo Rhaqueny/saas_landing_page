a07f9bbdfa55be6c8c188c4c38577288550af6dc
Rashadul Haque <rhaqueny@gmail.com>
2024-09-23 22:56:07 -0400

    Initial commit

---
+++ src/sections/Features.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Element } from 'react-scroll'
+
+const Features = () => {
+  return (
+    <section>
+        <Element name='features'></Element>
+    </section>
+  )
+}
+
+export default Features
