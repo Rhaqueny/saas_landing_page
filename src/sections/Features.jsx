a7f49e7127196796b3be873fc90a805b9d2683d3
Rashadul Haque <rhaqueny@gmail.com>
2024-09-25 22:00:06 -0400

    Initial commit

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Button, Element } from "react-scroll";
+import { Element } from "react-scroll";
 import { details, features } from "../constants";
+import Button from "../components/Button";
 
 const Features = () => {
   return (
@@ -54,16 +55,26 @@ const Features = () => {
 
               {details.map(({ id, icon, title }) => (
                 <li key={id} className="relative pt-16 px-4 pb-14">
-                  <div className="absolute top-0 bottom-0 left-1/2 
-                  bg-s3/20 w-[1px] h-full z-10" />
+                  <div
+                    className="absolute top-0 bottom-0 left-1/2 
+                  bg-s3/20 w-[1px] h-full z-10"
+                  />
 
-                  <div className="flex items-center justify-center mx-auto mb-3 border-2 border-s2 rounded-full hover:border-s4">
+                  <div
+                    className="flex items-center justify-center mx-auto 
+                  mb-3 border-2 border-s2 rounded-full hover:border-s4 
+                  transition-all duration-500 shadow-500"
+                  >
                     <img
                       src={icon}
                       alt={title}
                       className="size-17/20 object-contain z-20"
                     />
                   </div>
+                  <h3 className="relateive z-2 max-w-36 mx-auto my-0 
+                  base-small text-center uppercase">
+                    {title}
+                  </h3>
                 </li>
               ))}
             </ul>
