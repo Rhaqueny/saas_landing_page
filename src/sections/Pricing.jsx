2e0e28d6a99182509f174713bba6140c2e148a9c
Rashadul Haque <rhaqueny@gmail.com>
2024-10-01 23:23:07 -0400

    Initial commit

--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import clsx from "clsx";
 import CountUp from "react-countup";
 import { plans } from "../constants";
+import Button from "../components/Button";
 
 const Pricing = () => {
   const [monthly, setMonthly] = useState(false);
@@ -108,7 +109,7 @@ const Pricing = () => {
                 >
                   <div
                     className={clsx(
-                      "small-2 rounded-20 relative z-2 mx-auto mb-6 border-2 px-4 py-1.5 uppercased",
+                      "small-2 rounded-20 relative z-2 mx-auto mb-6 border-2 px-4 py-1.5 uppercase",
                       index === 1 ? "border-p3 text-p3" : "border-p1 text-p1"
                     )}
                   >
@@ -138,11 +139,40 @@ const Pricing = () => {
                 </div>
                 <div
                   className={clsx(
-                    "body-1 relative z-2 mb-10 w-full border-b-s2 pb-9 text-center text-p4"
+                    "body-1 relative z-2 mb-10 w-full border-b-s2 pb-9 text-center text-p4",
+                    index === 1 && "border-b"
                   )}
                 >
                   {plan.caption}
                 </div>
+
+                <ul className="mx-auto space-y-4 xl:px-7">
+                  {plan.features.map((feature) => (
+                    <li
+                      key={feature}
+                      className="relative flex items-center gap-5"
+                    >
+                      <img
+                        src="/images/check.png"
+                        alt="check"
+                        className="size-10 object-contain"
+                      />
+                      <p className="flex-1">{feature}</p>
+                    </li>
+                  ))}
+                </ul>
+                <div className="mt-10 flex w-full justify-center">
+                  <Button icon={plan.icon}>Get Started</Button>
+                </div>
+                {index === 1 && (
+                  <p
+                    className="small-compact mt-9 text-center 
+                  text-p3 before:mx-2.5 before:content-['-'] 
+                  after:mx-2.5 after:content-['-']"
+                  >
+                    Limited time offer
+                  </p>
+                )}
               </div>
             ))}
           </div>
