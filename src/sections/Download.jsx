96e2a8d5098dd81a29de418c93912342532092b6
Rashadul Haque <rhaqueny@gmail.com>
2024-10-03 14:56:12 -0400

    Initial commit

--- src/sections/Download.jsx
+++ src/sections/Download.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Element } from "react-scroll";
+import { links } from "../constants";
+import { Marker } from "../components/Marker.jsx";
 
 const Download = () => {
   return (
@@ -24,8 +26,40 @@ const Download = () => {
                 flavor, we've got you covered.
               </p>
               <ul className="flex flex-wrap items-center gap-6">
-
+                {links.map(({ id, url, icon }) => (
+                  <li
+                    key={id}
+                    className="download_tech-link 
+                    download_tech-link_last-before
+                    download_tech-link_last-after"
+                  >
+                    <a
+                      href={url}
+                      className="size-22 download_tech-icon_before relative
+                      flex items-center justify-center rounded-half border-2
+                      border-s3 bg-s1 transition-borderColor duration-500"
+                    >
+                      <span className="absolute -top-2 rotate-90">
+                        <Marker />
+                      </span>
+                      <img
+                        src={"/images/lines.svg"}
+                        alt="lines"
+                        className="absolute size-13/20 object-contain"
+                      />
+                      <span className="download_tech-icon">{icon}</span>
+                    </a>
+                  </li>
+                ))}
               </ul>
+            </div>
+            <div className="mb-10 max-md:hidden">
+              <div className="download_preview-before download_preview-after 
+              rounded-40 relative w-[955px] border-2 border-s5 p-6">
+                <div className="relative rounded-3xl bg-s1 px-6 pb-6 pt-14">
+
+                </div>
+              </div>
             </div>
           </div>
         </div>
